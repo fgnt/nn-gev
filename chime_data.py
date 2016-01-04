b97b2921165b7650ae8ffa6fd5ae2f71e4877e9c
Jahn Heymann <heymann@nt.upb.de>
2016-01-04 15:40:07 +0100

    Added functions to read data with context for real case

--- chime_data.py
+++ chime_data.py
@@ -11,20 +11,31 @@ from fgnt.signal_processing import stft
 from fgnt.utils import mkdir_p
 
 
-def _gen_flist_wo_ext(chime_data_dir, stage, scenario):
+def gen_flist_simu(chime_data_dir, stage):
     with open(os.path.join(
             chime_data_dir, 'annotations',
-            '{}05_{}.json'.format(stage, scenario))) as fid:
+            '{}05_{}.json'.format(stage, 'simu'))) as fid:
         annotations = json.load(fid)
+    isolated_dir = 'isolated' if stage == 'et' else 'isolated_ext'
     flist = [os.path.join(
-        chime_data_dir, 'audio', '16kHz', 'isolated_ext',
-        '{}05_{}_{}'.format(stage, a['environment'].lower(), scenario),
+        chime_data_dir, 'audio', '16kHz', isolated_dir,
+        '{}05_{}_{}'.format(stage, a['environment'].lower(), 'simu'),
         '{}_{}_{}'.format(a['speaker'], a['wsj_name'], a['environment']))
              for a in annotations]
     return flist
 
+def gen_flist_real(chime_data_dir, stage):
+    with open(os.path.join(
+            chime_data_dir, 'annotations',
+            '{}05_{}.json'.format(stage, 'real'))) as fid:
+        annotations = json.load(fid)
+    flist_tuples = [(os.path.join(
+        chime_data_dir, 'audio', '16kHz', 'embedded', a['wavfile']),
+                     a['start'], a['end'], a['wsj_name']) for a in annotations]
+    return flist_tuples
 
-def _get_audio_data(file_template, postfix='', ch_range=range(1, 7)):
+
+def get_audio_data(file_template, postfix='', ch_range=range(1, 7)):
     audio_data = list()
     for ch in ch_range:
         audio_data.append(audioread(
@@ -34,14 +45,28 @@ def _get_audio_data(file_template, postfix='', ch_range=range(1, 7)):
     return audio_data
 
 
+def get_audio_data_with_context(embedded_template, t_start, t_end,
+                                ch_range=range(1, 7)):
+    start_context = max((t_start-5), 0)
+    context_frames = (t_start - start_context) * 16000
+    audio_data = list()
+    for ch in ch_range:
+        audio_data.append(audioread(
+            embedded_template + '.CH{}.wav'.format(ch),
+        offset=start_context, duration=t_end-start_context)[None, :])
+    audio_data = np.concatenate(audio_data, axis=0)
+    audio_data = audio_data.astype(np.float32)
+    return audio_data, context_frames
+
+
 def prepare_training_data(chime_data_dir, dest_dir):
     for stage in ['tr', 'dt']:
-        flist = _gen_flist_wo_ext(chime_data_dir, stage, 'simu')
+        flist = gen_flist_simu(chime_data_dir, stage)
         export_flist = list()
         mkdir_p(os.path.join(dest_dir, stage))
         for f in tqdm.tqdm(flist, desc='Generating data for {}'.format(stage)):
-            clean_audio = _get_audio_data(f, '.Clean')
-            noise_audio = _get_audio_data(f, '.Noise')
+            clean_audio = get_audio_data(f, '.Clean')
+            noise_audio = get_audio_data(f, '.Noise')
             X = stft(clean_audio, time_dim=1).transpose((1, 0, 2))
             N = stft(noise_audio, time_dim=1).transpose((1, 0, 2))
             IBM_X, IBM_N = estimate_IBM(X, N)
