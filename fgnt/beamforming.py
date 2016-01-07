49f76bea961e778465b769046a195e74fc0282c4
Jahn Heymann <heymann@nt.upb.de>
2016-01-07 07:34:04 +0100

    Simplify psd matrix calculation for this special case of one speaker + noise
    Fix bug in gev_wrapper

--- fgnt/beamforming.py
+++ fgnt/beamforming.py
@@ -4,77 +4,27 @@ from scipy.linalg import eig
 from scipy.linalg import eigh
 
 
-def get_power_spectral_density_matrix(observation, mask=None, sensor_dim=-2,
-                                      source_dim=-2, time_dim=-1):
+def get_power_spectral_density_matrix(observation, mask=None):
     """
     Calculates the weighted power spectral density matrix.
-    It's also called covariance matrix.
 
-    With the *_dim parameters you can change the sort of the dims of the observation and mask.
-    But not every combination is allowed.
+    This does not yet work with more than one target mask.
 
-    :param observation: Complex observations with shape (..., sensors, frames)
-    :param mask: Masks with shape (bins, frames) or (..., sources, frames)
-    :param sensor_dim: change sensor diemension index (Default: -2)
-    :param source_dim: change source diemension index (Default: -2)
-    :param time_dim:  change time diemension index (Default: -1), this index must match for mask and observation
-    :return: PSD matrix with shape (..., sensors, sensors) or (..., sources, sensors, sensors)
-
-    Examples:
-    >>> F, T, D, K = 51, 31, 6, 2
-    >>> X = np.random.randn(F, D, T) + 1j * np.random.randn(F, D, T)
-    >>> mask = np.random.randn(F, K, T)
-    >>> mask = mask / np.sum(mask, axis=0, keepdims=True)
-    >>> get_power_spectral_density_matrix(X, mask=mask).shape
-    (51, 2, 6, 6)
-    >>> mask = np.random.randn(F, T)
-    >>> mask = mask / np.sum(mask, axis=0, keepdims=True)
-    >>> get_power_spectral_density_matrix(X, mask=mask).shape
-    (51, 6, 6)
+    :param observation: Complex observations with shape (bins, sensors, frames)
+    :param mask: Masks with shape (bins, frames) or (bins, 1, frames)
+    :return: PSD matrix with shape (bins, sensors, sensors)
     """
+    bins, sensors, frames = observation.shape
 
-    # Select the case you have specified
-    #   normalize mask is mask is not None
-    #   calc psd with einsum
     if mask is None:
-        if time_dim == -1 and sensor_dim == -2:
-            psd = np.einsum('...dt,...et->...de', observation,
-                            observation.conj())
-            psd /= observation.shape[-1]
-        elif time_dim == -2 and sensor_dim == -1:
-            psd = np.einsum('...td,...te->...de', observation,
-                            observation.conj())
-            psd /= observation.shape[-2]
-        elif time_dim == 0 and sensor_dim == 1:
-            psd = np.einsum('td...,te...->de...', observation,
-                            observation.conj())
-            psd /= observation.shape[0]
-        else:
-            print('time_dim: ', time_dim)
-            print('sensor_dim: ', sensor_dim)
-            print('observation.shape: ', observation.shape)
-            raise NotImplementedError()
-    else:
-        mask /= np.maximum(np.sum(mask, axis=time_dim, keepdims=True), 1e-10)
+        mask = np.ones((bins, frames))
+    if mask.ndim == 2:
+        mask = mask[:, np.newaxis, :]
 
-        if mask.ndim + 1 == observation.ndim:
-            mask = np.expand_dims(mask, sensor_dim)
-            source_dim = None
-        else:
-            mask = np.rollaxis(mask, source_dim, sensor_dim)
+    mask /= np.maximum(np.sum(mask, axis=-1, keepdims=True), 1e-6)
 
-        if time_dim == -1 and sensor_dim == -2:
-            psd = np.einsum('...kt,...dt,...et->...kde', mask, observation,
-                            observation.conj())
-            if source_dim is None:
-                psd = np.squeeze(psd, axis=-3)
-        elif time_dim == -2 and sensor_dim == -1:
-            psd = np.einsum('...tk,...td,...te->...kde', mask, observation,
-                            observation.conj())
-            if source_dim is None:
-                psd = np.squeeze(psd, axis=-3)
-        else:
-            raise NotImplementedError()
+    psd = np.einsum('...dt,...et->...de', mask * observation,
+                    observation.conj())
     return psd
 
 
@@ -97,7 +47,7 @@ def get_pca_vector(target_psd_matrix):
     vals = np.argmax(eigenvals, axis=-1)
     # Select eigenvec for max eigenval
     beamforming_vector = np.array(
-        [eigenvecs[i, :, vals[i]] for i in range(eigenvals.shape[0])])
+            [eigenvecs[i, :, vals[i]] for i in range(eigenvals.shape[0])])
     # Reconstruct original shape
     beamforming_vector = np.reshape(beamforming_vector, shape[:-1])
 
@@ -120,7 +70,7 @@ def get_mvdr_vector(atf_vector, noise_psd_matrix):
 
     # Make sure matrix is hermitian
     noise_psd_matrix = 0.5 * (
-    noise_psd_matrix + np.conj(noise_psd_matrix.swapaxes(-1, -2)))
+        noise_psd_matrix + np.conj(noise_psd_matrix.swapaxes(-1, -2)))
 
     numerator = solve(noise_psd_matrix, atf_vector)
     denominator = np.einsum('...d,...d->...', atf_vector.conj(), numerator)
@@ -156,10 +106,11 @@ def blind_analytic_normalization(vector, noise_psd_matrix):
     normalization = np.zeros(bins)
     for f in range(bins):
         normalization[f] = np.abs(np.sqrt(np.dot(
-            np.dot(np.dot(vector[f, :].T.conj(), noise_psd_matrix[f]),
-                   noise_psd_matrix[f]), vector[f, :])))
+                np.dot(np.dot(vector[f, :].T.conj(), noise_psd_matrix[f]),
+                       noise_psd_matrix[f]), vector[f, :])))
         normalization[f] /= np.abs(np.dot(
-            np.dot(vector[f, :].T.conj(), noise_psd_matrix[f]), vector[f, :]))
+                np.dot(vector[f, :].T.conj(), noise_psd_matrix[f]),
+                vector[f, :]))
 
     return vector * normalization[:, np.newaxis]
 
@@ -186,8 +137,6 @@ def gev_wrapper_on_masks(mix, noise_mask=None, target_mask=None,
 
     target_psd_matrix = get_power_spectral_density_matrix(mix, target_mask)
     noise_psd_matrix = get_power_spectral_density_matrix(mix, noise_mask)
-
-    target_psd_matrix -= noise_psd_matrix
 
     # Beamforming vector
     W_gev = get_gev_vector(target_psd_matrix, noise_psd_matrix)
