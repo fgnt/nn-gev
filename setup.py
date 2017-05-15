50a1dd612cc36043b3b822b340bc1a83f1991863
Christoph <cbj@mail.uni-paderborn.de>
2017-05-15 10:51:20 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="fgnt",
+
+    author="fgnt",
+    #author_email="Your email address",
+
+    description="A short description of the project.",
+    long_description=open('README.md', encoding='utf-8').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
